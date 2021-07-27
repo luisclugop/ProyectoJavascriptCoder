1363b3975234302094a8c811ef7407e8a2d2381e
Luisclugop <luisclugop@gmail.com>
2021-07-27 18:15:57 -0500

    Agregado Api, variables, y textContents de las mismas variables

--- js/app.js
+++ js/app.js
@@ -38,7 +38,17 @@ let fechaActual = document.getElementById("fechaActual");
 let fechaMañana = document.getElementById("fechaMañana");
 let fechaPasadoMañana = document.getElementById("fechaPasadoMañana");
 let mostrarClima = document.getElementById("mostrarClima");
+
+let gradosClima = document.getElementById("grados");
+let factorClima = document.getElementById("factor");
 let ciudad = document.getElementById("ciudad");
+
+let climaMax = document.getElementById("climaMax");
+let climaMin = document.getElementById("climaMin");
+let viento = document.getElementById("viento");
+let sensacionTer = document.getElementById("sensacionTermica");
+let humedad = document.getElementById("humedad");
+
 
 // Selectores------------------------------------
 
@@ -51,6 +61,9 @@ function guardarDatosClima(e){
 
 	let ciudadUsuario = document.getElementById("filtroSelect").value;
 	const ClimaUsuario = new Clima(ciudadUsuario, 18, "Parcialmente Nublado", fechaTexto(fecha));
+	// Url de conexion api concatenando el valor de la ciudad
+	const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ciudadUsuario+'&units=metric&lang=sp&appid=509a71114c79dce7fe4e067058ed0286';
+	console.log(url);
 
 // Selectores------------------------------------
 
@@ -62,7 +75,44 @@ function guardarDatosClima(e){
 		console.log(elemento);
 	});
 
+	// Imprime en mi id=ciudad lo que traiga mi Valor de ciudadUsuario
 	ciudad.textContent = ciudadUsuario;
+
+	// API CLIMA Openweathermap ---------------------
+	
+	// Conexion por fetch y agregar variables que se van a utilizar
+
+	fetch(url).then(response => response.json())
+	.then(data => {
+		let nombreCiudadData = data['name'];
+		let idClima = data['id'];
+
+		let temperatura = data['main']['temp'];
+		let sensacionTermica = data['main']['feels_like'];
+		let humedadClima = data['main']['humidity'];
+		let temperaturaMinima = data['main']['temp_min'];
+		let temperaturaMaxima = data['main']['temp_max'];
+
+		let statusClima = data['weather'];
+		let statusClimaDescripcion = data['weather'][0]['description'];
+		let iconClimaDescripcion = data['weather'][0]['icon'];
+
+		let vientoClima = data['wind']['speed'];
+		
+
+		// Imprimir los datos del clima - dependiendo de la respuesta del usuario
+		gradosClima.textContent = Math.round(temperatura);
+		factorClima.textContent = statusClimaDescripcion;
+		climaMax.textContent = `${Math.round(temperaturaMaxima)} °C /`;
+		climaMin.textContent = `${Math.round(temperaturaMinima)} °C`;
+		viento.textContent = `${vientoClima} km/h`;
+		sensacionTer.textContent = `${Math.round(sensacionTermica)} °C`;
+		humedad.textContent = `${humedadClima} %`;
+	})
+	.catch(error => alert("Nombre de la ciudad Invalido"));
+
+	// API CLIMA Openweathermap ---------------------
+
 }
 
 mostrarClima.addEventListener("click", guardarDatosClima);
@@ -71,7 +121,6 @@ mostrarClima.addEventListener("click", guardarDatosClima);
 
 // Funcion Fecha Actual -------------------------
 
-// Funcion
 function fechaTexto(fecha){
 
 	// Arreglos
